58d34a9889c4534d6140624fceac001b0a259779
MeklitC <meklitc4@gmail.com>
2023-05-24 07:47:50 +0000

    tested data sourcing for RAB use case

---
+++ datasourcing/UseCase_Rwanda_RAB/potato/get_geoSpatialRelativeHumidity_RAB_Potato_MC.R
@@ -0,0 +1,51 @@
+
+
+#################################################################################################################
+## source "get_rain_temp_summary.R" function and get rain and RelativeHumidity summary data 
+#################################################################################################################
+source("~/agwise/AgWise_Scripts/data_sourcing/get_geoSpatialRelativeHumidity_MC.R")
+
+
+
+#################################################################################################################
+## get daily Wind Speed
+## for trial sites from AgEra5
+#################################################################################################################
+trial_point_RH_AgEra <- get_RelativeHumidity_pointData(country = "Rwanda",  useCaseName = "RAB", Crop = "Potato", AOI = FALSE,
+                                             overwrite = TRUE, Planting_month_date = NULL,  Harvest_month_date = NULL, 
+                                             jobs=10, season=NULL, dataSource = "AgEra", ID = "TLID")
+
+
+#################################################################################################################
+## get daily Wind Speed 
+## for AOI from AgEra5 :: 
+#################################################################################################################
+
+AOI_point_RH_AgEra5 <- get_RelativeHumidity_pointData(country = "Rwanda",  useCaseName = "RAB", Crop = "Potato", AOI = TRUE,
+                                            overwrite = TRUE, season="season1", Planting_month_date = "02-05", 
+                                            Harvest_month_date = "06-05", jobs=10, dataSource = "AgEra", ID = NULL)
+
+
+
+#################################################################################################################
+## get Wind Speed summaries
+## for AOI from AgEra
+#################################################################################################################
+
+AOI_RH_summary_p1 <- get_RelativeHumidity_pointSummarydata(country = "Rwanda",  useCaseName = "RAB", Crop = "Potato", 
+                                                 AOI = TRUE, overwrite = TRUE, season="season1", Planting_month_date = "02-05", 
+                                                 Harvest_month_date = "06-05", jobs=10, dataSource = "AgEra", ID = NULL)
+
+
+#################################################################################################################
+## get Wind Speed summaries
+## for trial sites from AgEra
+#################################################################################################################
+
+trial_RH_summary_p1 <- get_RelativeHumidity_pointSummarydata(country = "Rwanda",  useCaseName = "RAB", Crop = "Potato", 
+                                                   AOI = FALSE, overwrite = TRUE, season=NULL, Planting_month_date = NULL, 
+                                                   Harvest_month_date = NULL, jobs=10, dataSource = "AgEra", ID = "TLID")
+
+
+
+
