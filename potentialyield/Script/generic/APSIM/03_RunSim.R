7ccd9e8c94fd41afb205ea72236a98bdfb8837ff
Patricia Moreno <l.p.moreno@cgiar.org>
2025-09-18 09:25:35 +0000

    Updated APSIM codes scripts for handling soil and path errors

--- potentialyield/Script/generic/APSIM/03_RunSim.R
+++ potentialyield/Script/generic/APSIM/03_RunSim.R
@@ -8,8 +8,98 @@ if(any(installed_packages == FALSE)){
 # load required packages
 invisible(lapply(packages_required, library, character.only = TRUE))
 
-# Recursive function to update LL values
-fix_AirDry_LL <- function(node) {
+
+# Recursive function to fix SAT values
+fix_SAT <- function(node, path, ext_i, PD = 2.65, epsilon = 1e-4) {
+  
+  soil_issues_file <- file.path(path, "soil_issues_log.txt")
+  
+  # Process Physical nodes
+  if (!is.null(node$Children) && node$`$type` == "Models.Soils.Physical, Models") {
+    
+    # Fix SAT <= 1 - BD / PD
+    if (!is.null(node$SAT) && !is.null(node$BD)) {
+      for (i in seq_along(node$SAT)) {
+        max_sat <- 1 - node$BD[[i]] / PD
+        if (node$SAT[[i]] > max_sat) {
+          alert_msg <- sprintf(
+            "%s/EXT %d Layer %d: SAT (%.4f) >= max SAT (%.4f). Adjusted to %.4f.",
+            basename(path), ext_i, i,
+            node$SAT[[i]], max_sat,
+            max_sat - epsilon
+          )
+          
+          node$SAT[[i]] <- max_sat - epsilon
+          
+          # Print to console
+          message(alert_msg)
+          # Append to log file
+          cat(alert_msg, file = soil_issues_file, append = TRUE, sep = "\n")
+          
+        }
+        
+      }
+    }
+  }
+  
+  # Recurse into children
+  if (!is.null(node$Children)) {
+    for (i in seq_along(node$Children)) {
+      node$Children[[i]] <- fix_SAT(node$Children[[i]], path = path, ext_i = ext_i, PD = PD, epsilon=epsilon)
+    }
+  }
+  
+  return(node)
+}
+
+
+# Fix and alert about invalid or wrong soil parameters
+fix_soil_pair <- function(node, higher, lower, path, ext_i, epsilon = 1e-4) {
+  
+  soil_issues_file <- file.path(path, "soil_issues_log.txt")
+  
+  
+  # Process Physical soil nodes
+  if (!is.null(node$Children) && node$`$type` == "Models.Soils.Physical, Models") {
+    
+    if (!is.null(node[[higher]]) && !is.null(node[[lower]])) {
+      for (i in seq_along(node[[lower]])) {
+        if (node[[lower]][[i]] >= node[[higher]][[i]]) {
+          alert_msg <- sprintf(
+            "%s/EXT %d Layer %d | %s (%.4f) >= %s (%.4f). Adjusted %s to %.4f.",
+            basename(path), ext_i, i,
+            lower, node[[lower]][[i]], higher, node[[higher]][[i]],
+            lower, node[[higher]][[i]] - epsilon
+          )
+          
+          # Print to console
+          message(alert_msg)
+          
+          # Append to log file
+          cat(alert_msg, file = soil_issues_file, append = TRUE, sep = "\n")
+          
+          # Apply the fix
+          node[[lower]][[i]] <- node[[higher]][[i]] - epsilon
+        }
+      }
+    }
+  }
+  
+  # Recurse into children
+  if (!is.null(node$Children)) {
+    for (i in seq_along(node$Children)) {
+      node$Children[[i]] <- fix_soil_pair(node$Children[[i]], higher, lower, path, ext_i, epsilon)
+    }
+  }
+  
+  return(node)
+}
+
+
+# Fix and alert about invalid or wrong cropLL values
+fix_AirDry_cropLL <- function(node, path, ext_i, epsilon = 1e-3) {
+  
+  soil_issues_file <- file.path(path, "soil_issues_log.txt")
   
   # Process Physical nodes
   if (!is.null(node$Children) && node$`$type` == "Models.Soils.Physical, Models") {
@@ -21,7 +111,34 @@ fix_AirDry_LL <- function(node) {
           # Make all values in AirDry <= LL
           for (i in seq_along(child$LL)) {
             if (node$AirDry[[i]] > child$LL[[i]]) {
-              node$AirDry[[i]] <- child$LL[[i]]
+              # Alert and adjust AirDry
+              alert_msg <- sprintf(
+                "%s/EXT %d Layer %d | AirDry (%.4f) > CropLL (%.4f). Adjusted AirDry to %.4f.",
+                basename(path), ext_i, i,
+                node$AirDry[[i]], child$LL[[i]],
+                child$LL[[i]] - epsilon
+              )
+              
+              message(alert_msg)
+              cat(alert_msg, file = soil_issues_file, append = TRUE, sep = "\n")
+              
+              node$AirDry[[i]] <- child$LL[[i]] - epsilon
+              
+              # --- NEW: Adjust LL15 if necessary ---
+              if (!is.null(node$LL15) && node$LL15[[i]] < node$AirDry[[i]]) {
+                alert_msg2 <- sprintf(
+                  "%s/EXT %d Layer %d | LL15 (%.4f) < modified AirDry (%.4f). Adjusted LL15 to %.4f.",
+                  basename(path), ext_i, i,
+                  node$LL15[[i]], node$AirDry[[i]],
+                  node$AirDry[[i]]
+                )
+                
+                message(alert_msg2)
+                cat(alert_msg2, file = soil_issues_file, append = TRUE, sep = "\n")
+                
+                node$LL15[[i]] <- node$AirDry[[i]]
+              }
+              # --- END NEW ---
             }
           }
         }
@@ -32,62 +149,8 @@ fix_AirDry_LL <- function(node) {
   # Recurse into children
   if (!is.null(node$Children)) {
     for (i in seq_along(node$Children)) {
-      node$Children[[i]] <- fix_AirDry_LL(node$Children[[i]])
-    }
-  }
-  
-  return(node)
-}
-
-
-# Function to ensure AirDry <=  LL15
-fix_AirDry_LL15 <- function(node, epsilon = 1e-4) {
-  
-  # Process Physical nodes
-  if (!is.null(node$Children) && node$`$type` == "Models.Soils.Physical, Models") {
-    
-    # Ensure AirDry <= LL15
-    if (!is.null(node$AirDry) && !is.null(node$LL15)) {
-      for (i in seq_along(node$AirDry)) {
-        if (node$AirDry[[i]] > node$LL15[[i]]) {
-          node$AirDry[[i]] <- node$LL15[[i]] - epsilon
-        }
-      }
-    }
-  }
-  
-  # Recurse into children
-  if (!is.null(node$Children)) {
-    for (i in seq_along(node$Children)) {
-      node$Children[[i]] <- fix_AirDry_LL15(node$Children[[i]], epsilon)
-    }
-  }
-  
-  return(node)
-}
-
-
-# Recursive function to fix SAT values
-fix_SAT <- function(node, PD = 2.65, epsilon = 1e-4) {
-  
-  # Process Physical nodes
-  if (!is.null(node$Children) && node$`$type` == "Models.Soils.Physical, Models") {
-    
-    # Fix SAT <= 1 - BD / PD
-    if (!is.null(node$SAT) && !is.null(node$BD)) {
-      for (i in seq_along(node$SAT)) {
-        max_sat <- 1 - node$BD[[i]] / PD
-        if (node$SAT[[i]] > max_sat) {
-          node$SAT[[i]] <- max_sat - epsilon
-        }
-      }
-    }
-  }
-  
-  # Recurse into children
-  if (!is.null(node$Children)) {
-    for (i in seq_along(node$Children)) {
-      node$Children[[i]] <- fix_SAT(node$Children[[i]], PD, epsilon)
+      node$Children[[i]] <- fix_AirDry_cropLL(node$Children[[i]], path, ext_i, 
+                                          epsilon = 1e-3)
     }
   }
   
@@ -148,20 +211,36 @@ fix_start_end_dates <- function(expfile_name, met_file, out_file = NULL){
 #'
 #' @examples
 
-runapsim <-function(i,path.to.extdata,expfile_name,AOI=TRUE){
-  setwd(paste(path.to.extdata,paste0('EXTE', formatC(width = 4, (as.integer(i)), flag = "0")), sep = "/"))
+runapsim <-function(i,path.to.zone,expfile_name,AOI=TRUE){
+  setwd(paste(path.to.zone, paste0('EXTE', formatC(width = 4, (as.integer(i)), flag = "0")), sep = "/"))
 
   # Run checks for the APSIM file
   apsim_data <- fromJSON(expfile_name, simplifyVector = FALSE)
   
-
-  fixed_data <- fix_AirDry_LL(apsim_data)
-  
-  fixed_data <- fix_AirDry_LL15(fixed_data)
-  
-  fixed_data <- fix_SAT(fixed_data,
+  # Ensure SAT has a valid value
+  # apsim_data$Children[[2]]$Children[[3]]$Children[[5]]$Children[[4]]$Children[[1]]$SAT
+  fixed_data <- fix_SAT(apsim_data,
+                        path = path.to.zone,
+                        ext_i = i, 
                         PD = 2.65,
                         epsilon = 1e-2)
+  # Ensure DUL < SAT
+  # fixed_data <- fix_SAT_DUL(fixed_data)
+  fixed_data <- fix_soil_pair(fixed_data, higher = "SAT", lower = "DUL", 
+                              path = path.to.zone, ext_i = i)
+  
+  # Ensure LL15 < DUL
+  apsim_data <- fix_soil_pair(fixed_data, higher = "DUL", lower = "LL15",
+                              path = path.to.zone, ext_i = i)
+  
+  # Ensure AirDry <= LL15
+  # fixed_data <- fix_AirDry_LL15(fixed_data)
+  apsim_data <- fix_soil_pair(fixed_data, higher = "LL15", lower = "AirDry",
+                              path = path.to.zone, ext_i = i)
+  
+  # Adjust AiDry downwards where CropLL < AirDry
+  fixed_data <- fix_AirDry_cropLL(fixed_data, path = path.to.zone, ext_i = i)
+  
 
   write_json(fixed_data, 
              expfile_name, 
@@ -187,32 +266,46 @@ apsim.exec <- function(country, useCaseName, Crop, AOI = TRUE,expfile_name,varie
   
   #Set working directory to save the results
   if(AOI == TRUE){
-    path.to.extdata_ini <- paste("/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/useCase_", country, "_",useCaseName, "/", Crop, "/transform/APSIM/AOI/",varietyid, sep="")
+    path.to.varietyid <- paste("/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/useCase_", country, "_",useCaseName, "/", Crop, "/transform/APSIM/AOI/",varietyid, sep="")
   }else{
-    path.to.extdata_ini <- paste("/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/useCase_", country, "_",useCaseName, "/", Crop, "/transform/APSIM/fieldData/",varietyid, sep="")
+    path.to.varietyid <- paste("/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/useCase_", country, "_",useCaseName, "/", Crop, "/transform/APSIM/fieldData/",varietyid, sep="")
   }
   
   
-  #define working path or path to run the model
-  # path.to.extdata in this script is not the same as in script 2
+  # define working path or path to run the model
   if(!is.na(level2) & !is.na(zone)){
-    path.to.extdata <- paste(path.to.extdata_ini,zone,level2, sep = "/")
+    path.to.zone <- paste(path.to.varietyid,zone,level2, sep = "/")
   }else if(is.na(level2) & !is.na(zone)){
-    path.to.extdata <- paste(path.to.extdata_ini,zone, sep = "/")
+    path.to.zone <- paste(path.to.varietyid,zone, sep = "/")
   }else if(!is.na(level2) & is.na(zone)){
     print("You need to define first a zone (administrative level 1) to be able to run the model for level 2 (administrative level 2). Process stopped")
     return(NULL)
   }else{
-    path.to.extdata <- path.to.extdata_ini
+    path.to.zone <- path.to.varietyid
   }
-  if (!dir.exists(file.path(path.to.extdata))){
+  if (!dir.exists(file.path(path.to.zone))){
     print("You need to create the input files (weather, soil and experimental data) before running the model. Process stopped")
     return(NULL)
   }
   
-  setwd(path.to.extdata)
+  setwd(path.to.zone)
   
-  folders <- list.dirs(".", full.names = FALSE, recursive = TRUE)
+  # APSIMX does not allow for white spaces in paths
+  safe.path.to.zone <- gsub(" ", "_", path.to.zone)
+  renamed_path <- FALSE
+  if (!dir.exists(safe.path.to.zone) && dir.exists(path.to.zone)) {
+    # Remove symbolic link if it exists
+    target <- Sys.readlink(safe.path.to.zone)
+    if (!is.na(target) & nzchar(target)){
+      unlink(safe.path.to.zone, recursive=TRUE)
+    }
+    file.rename(path.to.zone, safe.path.to.zone)
+    setwd(safe.path.to.zone)
+    renamed_path <- TRUE
+  }
+
+
+  folders <- list.dirs(".", full.names = FALSE, recursive = FALSE)
   folders <- grep(folders, pattern = ".ipynb", value = TRUE, invert = TRUE, fixed = TRUE)
   matching_folders <- folders[grepl("EXTE", folders, ignore.case = TRUE)]
   
@@ -221,25 +314,31 @@ apsim.exec <- function(country, useCaseName, Crop, AOI = TRUE,expfile_name,varie
   indices <- seq_along(matching_folders)
   
   # Create a log file to see the progress of the simulations
-  log_file <- paste(path.to.extdata,"progress_log_run.txt",sep='/')
+  log_file <- paste(safe.path.to.zone,"progress_log_run.txt",sep='/')
   if (file.exists(log_file)) {
     file.remove(log_file)
   }
   
   num_cores <- availableCores() -3
   plan(multisession, workers = num_cores)
-  results <- future_lapply(indices, function(i) {
+  # Removed storing results because they are being saved to a csv file
+  # results <- future_lapply(indices, function(i) {
+  future_lapply(indices, function(i) {
     message <- paste("Progress:", i, "out of", length(indices))
     cat(message, "\n", file = log_file, append = TRUE)
     
-    result <- runapsim(i, path.to.extdata=path.to.extdata,expfile_name=expfile_name,AOI=AOI)
+    # result <- runapsim(i, path.to.zone=path.to.zone,expfile_name=expfile_name,AOI=AOI)
+    runapsim(i, path.to.zone=safe.path.to.zone,expfile_name=expfile_name,AOI=AOI)
     
     message2 <- paste("Finished:", i, "out of", length(indices))
     cat(message2, "\n", file = log_file, append = TRUE)
     
-    return(result)
+    # return(result)
   })
   plan(sequential)
+  if (renamed_path){
+    file.rename(safe.path.to.zone, path.to.zone)
+  }
   rm(list = ls())
   gc()
   
@@ -273,8 +372,4 @@ apsim.exec <- function(country, useCaseName, Crop, AOI = TRUE,expfile_name,varie
 #   
 #    return(newlist)
 # }
-
-
-
-
 
--- potentialyield/Script/useCases/UseCase_Rwanda_RAB/Maize/APSIM/03_run_RAB_Maize_APSIM.R
+++ potentialyield/Script/useCases/UseCase_Rwanda_RAB/Maize/APSIM/03_run_RAB_Maize_APSIM.R
@@ -18,28 +18,23 @@ varietyid = "Early"
 level2=NA
 AOI = TRUE
 
+path.to.varietyid <- paste("/home/jovyan/agwise-potentialyield/dataops/potentialyield/Data/useCase_", 
+                         country, "_",useCaseName, "/", Crop, "/transform/APSIM/AOI/", varietyid, sep="")
 
+# This file will show soil parameters that had issues and were corrected
+soil_log_path <- paste(path.to.varietyid,"soil_issues_log.txt",sep='/')
+soil_log_file <- file.path(soil_log_path)
+if (file.exists(soil_log_file)) file.remove(soil_log_file)
 
 start_time <- Sys.time()
 for (i in 1:length(prov)){
-
+  
   apsim.exec(country=country,  useCaseName = useCaseName, Crop = Crop, AOI = AOI,
              expfile_name=expfile_name,varietyid=varietyid, zone=prov[i], level2=level2)
 
 }
 end_time <- Sys.time()
 duration <- end_time - start_time
-
-
-
-
-
-
-
-
-
-
-
-
+cat(duration)
 
 
